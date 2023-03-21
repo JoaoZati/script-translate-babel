6763f25e8b43120533c7c323eefc98b0cc75e348
joaozati <jzati@mitsidi.com>
2023-03-21 15:24:11 -0300

    Adicionando modificacao para automatizar palavra traduzida com varios paragramfos

--- main.py
+++ main.py
@@ -1,15 +1,42 @@
 from translate import Translator
 
-file_path_en = "/home/joaozati/Dev/brain/app/translations/en/LC_MESSAGES/messages.po"
-file_path_out_en = "/home/joaozati/Dev/brain/app/translations/en/LC_MESSAGES/messages_en.po"
-file_path_es = "/home/joaozati/Dev/brain/app/translations/es/LC_MESSAGES/messages.po"
-file_path_out_es = "/home/joaozati/Dev/brain/app/translations/es/LC_MESSAGES/messages_es.po"
-
-
 def split_and_append_from_caracter(str_new, str_file, caracter):
     lst_split = str_file.split(caracter)
     str_new += lst_split[0] + caracter
     str_file = caracter.join(lst_split[1:])
+
+    return str_new, str_file
+
+
+def split_translate_and_append_word(
+    str_new, str_file, str_palavra_a_traduzir, translator, translate_olds, number_print
+):
+    """
+
+    Return:
+    str_new: palavra terminando com 
+    """
+
+    lst_traducao = str_file.split('"\n\n')
+    str_palavra_traduzida = lst_traducao[0] + '"\n\n'
+
+    palavra_traducao = 'Traducão Pulada'
+    if str_palavra_traduzida in ["", '"\n', '"\n\n'] or translate_olds:
+        str_palavra_traduzida = translator.translate(
+            str_palavra_a_traduzir.replace('"', '\"').replace('\n', '\\n')
+        ) + "\\n"
+        str_palavra_traduzida = str_palavra_traduzida.replace('\\n', '\n')
+        palavra_traducao = 'Traduzido'
+
+    print(
+        f'{number_print}) {palavra_traducao}:|{str_palavra_a_traduzir}| - Para:|{str_palavra_traduzida}|'
+    )
+
+    if len(lst_traducao) < 2:
+        str_new += str_palavra_traduzida
+        return str_new, ""
+
+    str_file = str_palavra_traduzida + '"\n\n'.join(lst_traducao[1:])
 
     return str_new, str_file
 
@@ -32,29 +59,13 @@ def translate_word_from_old_to_new(
 
     str_new, str_file = split_and_append_from_caracter(str_new, str_file, 'msgid "')
 
-    str_palavra_a_traduzir = str_file.split('"')[0]
+    str_palavra_a_traduzir = str_file.split('msgstr "')[0]
 
     str_new, str_file = split_and_append_from_caracter(str_new, str_file, 'msgstr "')
 
-    lst_traducao = str_file.split('"')
-    str_palavra_traduzida = lst_traducao[0]
-
-    palavra_traducao = 'Traducão Pulada'
-    if not str_palavra_traduzida or translate_olds:
-        str_palavra_traduzida = translator.translate(str_palavra_a_traduzir)
-        palavra_traducao = 'Traduzido'
-
-    print(
-        f'{number_print}) {palavra_traducao}:|{str_palavra_a_traduzir}| - Para:|{str_palavra_traduzida}|'
+    return split_translate_and_append_word(
+        str_new, str_file, str_palavra_a_traduzir, translator, translate_olds, number_print
     )
-
-    if len(lst_traducao) <= 2:
-        str_new += str_palavra_traduzida + '"'
-        return str_new, ""
-
-    str_file = str_palavra_traduzida + '"' + '"'.join(lst_traducao[1:])
-
-    return str_new, str_file
 
 
 def generate_message_po(file_path, file_path_out, to_lang, from_lang='pt', translate_olds=False):
@@ -63,7 +74,7 @@ def generate_message_po(file_path, file_path_out, to_lang, from_lang='pt', trans
     with open(file_path, 'r') as file:
         str_file = file.read()
 
-    # Coping init of file_string in a new string with caracter '#:'
+    # Coping init of file_string in a new string with caracter '#:' (to jump first msgid)
     str_new, str_file = split_and_append_from_caracter('', str_file, '#:')
     count = 0
     while True:
@@ -80,9 +91,14 @@ def generate_message_po(file_path, file_path_out, to_lang, from_lang='pt', trans
 
 
 if __name__=='__main__':
+    file_path_en = "/home/joaozati/Dev/brain/app/translations/en/LC_MESSAGES/messages.po"
+    file_path_out_en = "/home/joaozati/Dev/brain/app/translations/en/LC_MESSAGES/messages_en.po"
+    file_path_es = "/home/joaozati/Dev/brain/app/translations/es/LC_MESSAGES/messages.po"
+    file_path_out_es = "/home/joaozati/Dev/brain/app/translations/es/LC_MESSAGES/messages_es.po"
+
     print('#'*100)
     print('Traduzindo messages_en.po')
     generate_message_po(file_path_en, file_path_out_en, 'en', from_lang='pt')
-    print('#'*100)
-    print('Traduzindo messages_es.po')
-    generate_message_po(file_path_es, file_path_out_es, 'es', from_lang='pt')
+    # print('#'*100)
+    # print('Traduzindo messages_es.po')
+    # generate_message_po(file_path_es, file_path_out_es, 'es', from_lang='pt')
